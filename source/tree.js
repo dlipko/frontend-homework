e6e655cf29760ad133f561404ba9148078ff908f
dlipko <lipko_dima@mail.ru>
2018-09-02 23:33:53 +0200

    repeat, let/const, кодстайл
    Постарался исправить кодстайл. Воспользовался стрелочной функцией, let/const, repeat

--- source/tree.js
+++ source/tree.js
@@ -1,19 +1,18 @@
-function tree(n) {
-	if (!Number(n) || n < 3 || n == null) 
+const tree = n => {
+	if (!Number(n) || n < 3 || n == null) {
 		return null;
+	}
 
-	var level = n;
-	var tree, space, branch, trunk;
-	[tree, space, branch, trunk, backSpace] = ['', ' ', '*', '|', "\n"];
+	let [level, tree] = [n, ''];
+	const [space, branch, trunk, backSpace] = [' ', '*', '|', '\n'];
 	
 	while (--level) {
-		// создаю строку джойном пустого массива нужным символом
-		var freeSpace = Array(level).join(space);
-		tree +=  freeSpace + Array(2*(n - level)).join(branch) + freeSpace + backSpace;
+		const freeSpace = space.repeat(level - 1);
+		tree +=  freeSpace + branch.repeat(2 * (n - level) - 1) + freeSpace + backSpace;
 	}
 
 	// добавляю ствол
-	var freeSpace = Array(n - 1).join(space);
+	const freeSpace = space.repeat(n - 2);
 	tree += freeSpace + trunk + freeSpace + backSpace;
 
 	return tree;
